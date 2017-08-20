5b012ad481ff2f67a613567049f73487d53bf8b3
Marc Reichelt <mcreichelt@gmail.com>
2017-08-20 23:49:38 +0200

    add latency measurement capability

--- src/main.cpp
+++ src/main.cpp
@@ -35,9 +35,11 @@ int main() {
 
     // MPC is initialized here!
     MPC mpc;
+    vector<double> latencies;
+    latencies.reserve(10000);
 
-    h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                       uWS::OpCode opCode) {
+    h.onMessage([&mpc, &latencies](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+                                   uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
         // The 2 signifies a websocket event
@@ -55,25 +57,19 @@ int main() {
                     double py = j[1]["y"];
                     double psi = j[1]["psi"];
                     double v = j[1]["speed"];
+                    auto time = chrono::steady_clock::now();
 
                     // convert absolute waypoints to relative ones using the vehicle's location and orientation
                     RelativeWaypoints trackWaypoints = transformToRelative(trackAbsolute, {px, py}, psi);
 
-                    /*
-                    * TODO: Calculate steering angle and throttle using MPC.
-                    *
-                    * Both are in between [-1, 1].
-                    *
-                    */
-
                     // first we fit a 3rd order polynomial
                     VectorXd coeffs = polyfit(vecXd(trackWaypoints.x), vecXd(trackWaypoints.y), 3);
-                    cout << "Polynom: y = " << coeffs[0] << " + " << coeffs[1] << " * x + " << coeffs[2] << " * x^2 + "
-                         << coeffs[3] << " * x^3" << endl;
+//                    cout << "Polynom: y = " << coeffs[0] << " + " << coeffs[1] << " * x + " << coeffs[2] << " * x^2 + "
+//                         << coeffs[3] << " * x^3" << endl;
 
                     double cte = polyeval(coeffs, 0);
                     double epsi = -(atan(polyeval(derivative(coeffs), 0)));
-                    cout << "CTE = " << cte << ", epsi = " << epsi << endl;
+//                    cout << "CTE = " << cte << ", epsi = " << epsi << endl;
 
 
                     // now we build our state which consists of 6 values: x, y, psi, v and error values cte end epsi
@@ -118,6 +114,14 @@ int main() {
                     // SUBMITTING.
                     // TODO: enable sleep again
 //                    this_thread::sleep_for(chrono::milliseconds(100));
+
+                    double latency = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - time)
+                                             .count() / 1000.0;
+                    double average = accumulate(latencies.begin(), latencies.end(), 0.0) / latencies.size();
+                    latencies.push_back(latency);
+                    cout << "latency: " << fixed << setprecision(3) << latency
+                         << " seconds, average: " << average << endl;
+
                     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
                 }
             } else {
